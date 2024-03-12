2b1ec2e8ffdd7b2a118ab23bd0f2fa2b65cd7b02
B33blebroxx <b33blebroxx@gmail.com>
2024-03-12 18:31:29 -0500

    File setup and RareUser model

---
+++ Models/Post.cs
@@ -0,0 +1,6 @@
+﻿namespace RareBE.Models
+{
+    public class Post
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
+using RareBE.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Extensions.Hosting;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+builder.Services.AddNpgsql<RareBEDbContext>(builder.Configuration["RareBEDbConnectionString"]);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000", "http://localhost:5003")
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
 
 var app = builder.Build();
+app.UseCors();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
