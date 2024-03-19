0717285ab3c619bf47d2cc02aff8905aa1514d8a
Dylan Moore <dylankmoore@gmail.com>
2024-03-19 18:38:27 -0500

    edited get user's post endpoint

--- Controllers/PostsApi.cs
+++ Controllers/PostsApi.cs
@@ -13,7 +13,7 @@ namespace RareBE.Controllers
             app.MapGet("/posts", (RareBEDbContext db) =>
             {
                 var posts = db.Posts
-                    .Where(p => p.Approved && p.PublicationDate <= DateTime.Now)
+                    .Where(p => p.Approved && p.PublicationDate <= DateTime.UtcNow)
                     .OrderByDescending(p => p.PublicationDate)
                     .Select(p => new {
                         p.Id,
@@ -101,6 +101,17 @@ namespace RareBE.Controllers
                 var posts = db.Posts
                 .Where(p => p.RareUserId == userId && p.Approved && p.PublicationDate <= DateTime.UtcNow)
                 .OrderByDescending(p => p.PublicationDate)
+                .Select(p => new {
+                    p.Id,
+                    p.Title,
+                    PublicationDate = p.PublicationDate.ToUniversalTime().ToString("MM/dd/yyyy hh:mm tt"),
+                    p.ImageUrl,
+                    p.Content,
+                    AuthorDisplayName = db.RareUsers
+            .Where(u => u.Id == p.RareUserId)
+            .Select(u => u.FirstName + " " + u.LastName)
+            .FirstOrDefault()
+                })
                 .ToList();
 
                 return Results.Ok(posts);
